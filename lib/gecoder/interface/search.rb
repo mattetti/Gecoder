e64230768850752ecad3ba9324c6334d8222d09c
lokorin <lokorin@475dfad3-6c2e-40b3-bf4b-5039957d4c96>
2007-07-19 20:28:34 +0000

    DFS now also runs with garbage collecting disabled.

--- lib/gecoder/interface/search.rb
+++ lib/gecoder/interface/search.rb
@@ -4,7 +4,9 @@ module Gecode
     # to that solution. Returns the model if a solution was found, nil 
     # otherwise.
     def solve!
+      GC.disable
       space = dfs_engine.next
+      GC.enable
       return nil if space.nil?
       @active_space = space
       return self
@@ -30,9 +32,11 @@ module Gecode
     # Yields each solution that the model has.
     def each_solution(&block)
       dfs = dfs_engine
+      GC.disable
       while not (@active_space = dfs.next).nil?
         yield self
       end
+      GC.enable
       self.reset!
     end
     
