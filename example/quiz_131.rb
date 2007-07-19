282fe6bf2b5c969d5d5899ed44aecd7e0f6f0d58
lokorin <lokorin@475dfad3-6c2e-40b3-bf4b-5039957d4c96>
2007-07-19 13:10:59 +0000

    A reworked example that "should" work, either contains an error or reveals some sort of bug.

--- example/quiz_131.rb
+++ example/quiz_131.rb
@@ -6,24 +6,28 @@ require File.dirname(__FILE__) + '/example_helper'
 class MaxSlice < Gecode::Model
   attr :substitutions
   attr :indices
+  attr :start
+  attr :length
   
   def initialize(array)
     @array = array
-    start, stop = int_var_array(2, 0...array.size)
-    start.must <= stop
+    @start, @length = int_var_array(2, 0...array.size)
+    (@start + @length).must < array.size
     
     # Sumation substitutions.
     @substitutions = Hash[*(0...array.size).zip(array).flatten]
-    
+
     # Set up a set of indices.
     @indices = set_var([], 0...array.size)
-    @indices.min.must == start
-    @indices.max.must == stop
+    @indices.min.must == @start
+    @indices.max.must == @start + @length - 1
+
+    @indices.size.must == @length
 
     # Implied constraints.
-    #@indices.sum(:substitute => @substitutions).must >= 0
+    @indices.sum(:substitutions => @substitutions).must >= 0
     
-    branch_on wrap_enum([start, stop])
+    branch_on wrap_enum([@start, @length])
   end
   
   def sum
@@ -31,12 +35,20 @@ class MaxSlice < Gecode::Model
   end
   
   def value
-    @array.slice(start.value, stop.value)
+    @array.slice(@start.value..@stop.value)
   end
 end
 
 array = [2,4,-5,2]
 solution = MaxSlice.new(array).optimize! do |model, best_so_far|
+p best_so_far.sum
+p best_so_far.start.value
+p best_so_far.length.value
   model.indices.sum(:substitutions => model.substitutions).must > best_so_far.sum
 end
-puts solution.value
+p solution.sum
+solution = MaxSlice.new(array).each_solution do |solution|
+p "one solution: #{solution.sum}"
+end
+#p solution.indices.value.to_a
+#p solution.value
